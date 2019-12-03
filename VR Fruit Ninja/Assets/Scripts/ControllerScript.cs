338b3d54bb5a0ef467613a5e8c0732fc44d8a5d9
Kimia K. Ahrabi <kimia96@gmail.com>
2019-12-03 16:33:22 -0500

    testing controller script
    testing controller script

---
+++ VR Fruit Ninja/Assets/Scripts/ControllerScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControllerScript : MonoBehaviour
+{
+    public GameObject go;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+        //sets the rotation of the controller 
+        transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+        if (Physics.Raycast(transform.position, transform.forward, out hit))
+        {
+            if (hit.collider != null)
+            {
+                if (go != hit.collider.gameObject)
+                {
+                    go = hit.transform.gameObject;
+                    Debug.Log("on VR raycast enter");
+                }
+            }
+        }
+    }
+}
