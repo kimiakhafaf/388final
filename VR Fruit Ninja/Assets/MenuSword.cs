97a4fe6ad28048b985e6c192ab5c8d067b98d612
bchen49 <benchen4998@gmail.com>
2019-12-06 12:11:31 -0500

    Created new MainMenu

---
+++ VR Fruit Ninja/Assets/MenuSword.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class MenuSword : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = OVRInput.GetLocalControllerRotation(OVRInput.Controller.RTrackedRemote);
+    }
+
+     private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "fruit")
+        {
+            SceneManager.LoadScene("game");
+        }
+    }
+}
