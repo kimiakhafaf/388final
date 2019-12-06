e1de6ee02573282ea9b1d0c7507552bf2c9cf2e3
Benjamin Chen <benchen4998@gmail.com>
2019-12-06 01:03:24 -0500

    Minor updates to fruit_fall script

--- VR Fruit Ninja/Assets/Scripts/fruit_fall.cs
+++ VR Fruit Ninja/Assets/Scripts/fruit_fall.cs
@@ -10,7 +10,13 @@ public class fruit_fall : MonoBehaviour
     public GameObject strawberry;
     public float spawnTime = .1f;            // How long between each spawn.
     private float timer = 0; //counting timer, reset after calling SpawnRandom() function
-
+    public List<GameObject> fruitList;
+    void Start(){
+        fruitList.Add(banana);
+        fruitList.Add(apple);
+        fruitList.Add(pear);
+        fruitList.Add(strawberry);
+    }
     void Update()
     {
         timer += Time.deltaTime;   
@@ -22,14 +28,23 @@ public class fruit_fall : MonoBehaviour
     }
     public void SpawnRandom()
     {
-        Instantiate(banana, new Vector3(Random.Range(-2, 2), Random.Range(5, 10) , Random.Range(4, 6)), Quaternion.identity);
-        Instantiate(apple, new Vector3(Random.Range(-2, 2), Random.Range(5, 10), Random.Range(4, 6)), Quaternion.identity);
-        Instantiate(pear, new Vector3(Random.Range(-2, 2), Random.Range(5, 10), Random.Range(4, 6)), Quaternion.identity);
-        Instantiate(strawberry, new Vector3(Random.Range(-2, 2), Random.Range(5, 10), Random.Range(4, 6)), Quaternion.identity);
-        Instantiate(banana, new Vector3(Random.Range(-2, 2), Random.Range(10, 15), Random.Range(4, 6)), Quaternion.identity);
-        Instantiate(apple, new Vector3(Random.Range(-2, 2), Random.Range(10, 15), Random.Range(4, 6)), Quaternion.identity);
-        Instantiate(pear, new Vector3(Random.Range(-2, 2), Random.Range(5, 10), Random.Range(4, 6)), Quaternion.identity);
-        Instantiate(strawberry, new Vector3(Random.Range(-2, 2), Random.Range(5, 10), Random.Range(4, 6)), Quaternion.identity);
+
+        for(int i = 0;i < Random.Range(2,5);i++){
+
+            int spawn = Random.Range(0,fruitList.Count);
+            Instantiate(fruitList[spawn],new Vector3(generateX(),generateY(),generateZ()),Quaternion.identity);
+        }
         
+    }
+
+    //Play with these numbers, not sure what the right range is
+    public float generateX(){
+        return Random.Range(-3f,3f);
+    }
+    public float generateY(){
+        return Random.Range(5f,10f);
+    }
+    public float generateZ(){
+        return Random.Range(2f,3f);
     }
 }
